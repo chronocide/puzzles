8385ca5b02fcfc18aec496b7d285024499f76d3c
Jason Hamje <chronocide@Aoede.(none)>
2014-07-13 16:28:07 -0500

    correct typo in example comment

--- max_array_gap.rb
+++ max_array_gap.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 # Given an Array A[], find the maximum j - i such that A[j] > A[i]. 
-# For example Input {34,8,10,3,2,80,30,33,1} and Output 6 (j=7(3), i=1(8)) 
+# For example Input {34,8,10,3,2,80,30,33,1} and Output 6 (j=7(33), i=1(8)) 
 # Time Complexity should be less then O(n^2)
 
 class Array
